df91e1a7cd3f7f10f8cbcc94190ba36f7cbc65ff
Jekz288 <dxakykroco@gmail.com>
2025-08-30 12:48:19 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+} = require("@whiskeysockets/baileys")
+const P = require("pino")
+const chalk = require("chalk")
+
+async function startSock() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info")
+  const { version } = await fetchLatestBaileysVersion()
+
+  const sock = makeWASocket({
+    version,
+    auth: state,
+    logger: P({ level: "silent" }),
+    printQRInTerminal: true
+  })
+
+  sock.ev.on("creds.update", saveCreds)
+
+  console.log(chalk.redBright(`
+=================================================
+🔥 TANJIRO CRASHER (Baileys Rynzz) 🔥
+✅ Bot Berhasil berjalan, Terimaksih sudah memakai bayleys bang rynzz
+=================================================
+  `))
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const m = messages[0]
+    if (!m.message) return
+    const text =
+      m.message.conversation ||
+      m.message.extendedTextMessage?.text ||
+      ""
+
+    if (text.toLowerCase() === ".ping") {
+      await sock.sendMessage(m.key.remoteJid, { text: "🏓 Pong! Bot aktif" })
+    }
+
+    if (text.toLowerCase() === ".menu") {
+      await sock.sendMessage(m.key.remoteJid, {
+        text: `
+📌 *TANJIRO BOT MENU*
+1. .menu → menampilkan menu
+2. .ping → cek status bot
+        `,
+      })
+    }
+  })
+}
+
+startSock()
+
+process.on("uncaughtException", console.error)
+process.on("unhandledRejection", console.error)
